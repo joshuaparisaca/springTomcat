e7e7787fbfb875f227022d79c047fe5c7a8f0575
Joshua Parisaca <joshuaparisaca@gmail.com>
2018-12-03 17:00:20 -0500

    .

--- image_versioning.sh
+++ image_versioning.sh
@@ -4,6 +4,6 @@ version=$((version+1))
 
 docker build -t tomcat_demo .
 
-docker tag tomcat_demo marc/test:$version 
+docker tag tomcat_demo tomcat_demo:$version 
 
 docker run -d -p 8888:8080 tomcat_demo:$version

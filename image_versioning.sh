bab76ac4e58e4c4defefe1b9dbff2fed8b60db1c
Joshua Parisaca <joshuaparisaca@gmail.com>
2018-12-03 17:02:21 -0500

    Figure out how to fix version control

--- image_versioning.sh
+++ image_versioning.sh
@@ -2,8 +2,6 @@
 
 version=$((version+1))
 
-docker build -t tomcat_demo .
-
-docker tag tomcat_demo tomcat_demo:$version 
+docker build -t tomcat_demo:$version .
 
 docker run -d -p 8888:8080 tomcat_demo:$version
